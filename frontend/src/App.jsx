8baca2525da4c7fc3343398500e4eadea93d2173
jashrashne <jash.rashne@gmail.com>
2024-10-27 00:25:42 +0530

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,17 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Yoga from "./components/Yoga";
-import View360 from "./components/View360";
-import MotionDetectionDrums from "./components/MotionDetectionDrums";
-import FallingRodsGame from "./components/FallingRodsGame";
-import Game from "./components/Game";
-import Login from "./components/Login";
-import FitData from "./components/FitData";
-import Layout from "./components/Layout";
-import LandingPage from "./components/LandingPage";
-import Game from "./components/Game";
-import GamePage from "./components/GamePage";
+import Game from "./components/MobilityGame/Game"
 
 function App() {
   const [count, setCount] = useState(0);
